6e738f309996b77a96ccab64ddf5db586c7db933
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-16 14:04:53 +0530

    Printingoutput.cpp

---
+++ printingoutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Generates a bell sound
+    cout << "My mobile number is "
+            "7\a8\a7\a3\a9\a2\a3\a4\a0\a8\a";
+    return 0;
+}
+//output
+My mobile number is 7873923408
