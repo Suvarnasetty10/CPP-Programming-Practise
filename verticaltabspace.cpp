3f4d60f62c59a9b22bf70eb30676ccb2a98a3dda
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-16 14:33:36 +0530

    Verticaltabspace.cpp

---
+++ verticaltabspace.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Inserts a vertical tab space
+    cout << "Hello friends\v";
+    cout << "Welcome to GFG";
+    return 0;       
+}
+//output:
+Hello friends
+             Welcome to GFG
