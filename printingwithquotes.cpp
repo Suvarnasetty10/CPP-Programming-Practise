248a54cf39e3ddf38136d13063cd862a2701578e
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-16 14:30:50 +0530

    Printingwithquotes.cpp

---
+++ printingwithquotes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Displays a single quotation mark
+    cout << "\' Hello Geeks\'\n";
+    // Displays double quotation marks
+    cout << "\" Hello Geeks\"";
+    return 0;
+}
+//output:
+' Hello Geeks'
+" Hello Geeks"
