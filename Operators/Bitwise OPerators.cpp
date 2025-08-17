8c92c41c36b57de8932c822ccc9c3e22fca59241
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-17 22:45:43 +0530

    Bitwise OPerators.cpp

---
+++ Operators/Bitwise OPerators.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+// Bitwise Operators.
+{
+	int a=4;
+	int b=10;
+	int c=1;
+	cout<<"a&b ="<<(a&b)<<endl;
+	cout<<"a|b ="<<(a|b)<<endl;
+	cout <<"a^b =" << (a^b)<<endl;
+	cout << "a<< 1 = " << (a<<1)<<endl;//leftshift
+	cout << "b>>1 = " << (b>>1)<<endl;//Right Shift
+	 cout << "~(a) = " << ~(a);
+	//cout<<"a&&b ="<<(a&&b)<<endl;
+	
+}
+//Output:
+a&b =0
+a|b =14
+a^b =14
+a<< 1 = 8
+b>>1 = 5
+~(a) = -5
