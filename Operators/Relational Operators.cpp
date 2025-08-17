ebfaa3a7f8bd29394cc547955f1890ba3d7ed895
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-17 22:18:40 +0530

    Relational Operators.cpp

---
+++ Operators/Relational Operators.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+//Relational Operators.
+{
+	int a=10;
+	int b=10;
+	cout<<"a==b =" <<(a==b)<<endl;
+	cout <<"a>b ="<<(b>a)<<endl;
+	cout<<"a<b ="<<(a<b)<<endl;
+	cout <<"a<=b ="<<(a<=b)<<endl;
+	cout <<"b>=a ="<<(b>=a)<<endl;
+	cout<<"a!b ="<<(a!=b)<<endl;
+}
+//output:
+a==b =1
+a>b =0
+a<b =0
+a<=b =1
+b>=a =1
+a!b =0
