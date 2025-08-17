333cc30c32ee11d63a42a38dd8f58a9040fc5442
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-17 22:33:57 +0530

    Logical Operators.cpp

---
+++ Operators/Logical Operators.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+//Logical Operators.
+{
+	int a=0;
+	int b=10;
+	int c=1;
+	cout<<"a&&b ="<<(a&&b)<<endl;
+	cout<<"a||b ="<<(a||b)<<endl;
+	cout << "!b is " << (!b)<<endl;
+	cout << "!a is " << (!a)<<endl;
+	cout << "c is " << (!c);
+	//cout<<"a&&b ="<<(a&&b)<<endl;
+	
+}
+//output:
+a&&b =0
+a||b =1
+!b is 0
+!a is 1
+c is 0
