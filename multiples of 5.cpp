2b4e50eb5f7eb91e637634abed85a4d5dd73cd2d
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-22 06:08:42 +0530

    multiples of 5.cpp

---
+++ multiples of 5.cpp
@@ -0,0 +1,14 @@
+//code for 5 multiples
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i,n;
+	cin>>n;
+	for(i=1;i<=10;i++)
+	{
+		cout<<n*i<<" ";
+	}
+}
+//output:
+5 10 15 20 25 30 35 40 45 50
