db0b3a2eefa201e85a13ffccc9a53a50ef82cc93
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-23 20:10:57 +0530

    code.cpp

---
+++ squareroot/code.cpp
@@ -0,0 +1,17 @@
+//code:
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,y;
+	    cin>> n;
+	    y=sqrt(n);
+	    cout<<y<<endl;
+	    
+	}
+}
