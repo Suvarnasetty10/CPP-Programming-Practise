a2ab47ba4ec618988414c157a707197f37ffbb52
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-08-24 16:29:17 +0530

    Infiniteloop.cpp

---
+++ Infiniteloop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // This is an infinite for loop as the condition
+    // expression is blank
+    for (;;) {
+        cout << "i love u forever.\n";
+    }
+	return 0;
+}
+//output:
+//prints i love u forever infinitly.
