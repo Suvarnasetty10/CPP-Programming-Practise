554c5d909726ec7f5486d5e680894a02a5bc2eb0
K.Suvarna Subramaneswari <166210217+Suvarnasetty10@users.noreply.github.com>
2025-09-01 20:18:47 +0530

    code.cpp

---
+++ Possible victory/code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+
+	    int r,o,c,i,g,h,p;
+	    cin>>r>>o>>c;
+	    h=20-o;
+	    p=h*6;
+	    g=p*6;
+	    i=g+c;
+	    if(i>r)
+	    {
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	
